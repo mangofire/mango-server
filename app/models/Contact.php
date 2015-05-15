569f51d9bc65e498b170ff8ee43406a11d7f86a5
张志强 <464511712@qq.com>
2015-05-15 13:14:15 +0800

    Revert "init"
    This reverts commit 799e95073ebbaa1e3f4a943ad5dd82d0ccf09743.

--- app/models/Contact.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Contact extends Eloquent{
-
-	protected $table = 'contacts';
-	protected $fillable = array('uid','mobile', 'name', 'group');
-}
--- app/models/SMS.php
+++
@@ -1,6 +0,0 @@
-<?php
-class SMS extends Eloquent{
-
-	protected $table = 'SMS';
-	protected $fillable = array('uid', 'content', 'type', 'address', 'time', 'is_read', 'created_at', 'updated_at');
-}
